9e289eb0e0d1e25256d97e4e3a640c5114fd3c53
arshilkhann__ <arshilkhan529@gmail.com>
2026-07-10 21:43:09 +0530

    Time: 5 ms (80.32%), Space: 46 MB (33.98%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,8 +1,5 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x < 0){
-            return false;
-        }
         if (x<0)return false;
         int z = x;
         int rev=0;
