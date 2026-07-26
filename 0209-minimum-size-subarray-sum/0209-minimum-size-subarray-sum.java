773cb9b9f52a8a46b526465600292d6fa1c2d829
arshilkhann__ <arshilkhan529@gmail.com>
2026-07-26 20:49:52 +0530

    Time: 1 ms (99.94%), Space: 54.2 MB (99.99%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,6 +1,6 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        int res = Integer.MAX_VALUE;
+       /* int res = Integer.MAX_VALUE;
         int sum =0;
 
         int low=0, high =0;
@@ -15,5 +15,24 @@ class Solution {
             high++;
         }
         return res == Integer.MAX_VALUE ? 0 : res;
+        */
+
+
+
+        int left =0;
+        int sum=0;
+        int minlen=Integer.MAX_VALUE;
+
+        for(int right=0;right<nums.length;right++){
+            sum +=nums[right];
+
+            while(sum>=target){
+                minlen=Math.min(minlen,right-left+1);
+                sum -=nums[left];
+                left++;
+            }
+        }
+        System.gc();
+        return minlen==Integer.MAX_VALUE ?0:minlen;
     }
 }
