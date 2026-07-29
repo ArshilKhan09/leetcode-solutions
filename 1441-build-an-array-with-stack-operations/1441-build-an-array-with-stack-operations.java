10f565568ca71fc7e070f4d1a229016dc4af9b13
arshilkhann__ <arshilkhan529@gmail.com>
2026-07-29 20:37:39 +0530

    Time: 0 ms (100%), Space: 44.4 MB (16.04%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
@@ -1,33 +1,19 @@
 class Solution {
     public List<String> buildArray(int[] target, int n) {
         List<String> ans = new ArrayList<>();
-        List<Integer> stack = new ArrayList<>();
-
         int index = 0;
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n && index < target.length; i++) {
 
-            stack.add(i);
             ans.add("Push");
 
-            if (index < target.length && i == target[index]) {
+            if (i == target[index]) {
                 index++;
             } else {
-                stack.remove(stack.size() - 1);
                 ans.add("Pop");
             }
-
-            if (stack.equals(toList(target)))
-                break;
         }
 
         return ans;
-    }
-
-    private List<Integer> toList(int[] target) {
-        List<Integer> list = new ArrayList<>();
-        for (int x : target)
-            list.add(x);
-        return list;
     }
 }
